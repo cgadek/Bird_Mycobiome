ccf3b96fc5c66a78e0dfd881f87f7ce9f54feda7
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2025-01-12 11:30:04 -0800

    Create fungal_phylo.sh

---
+++ fungal_phylo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBARCH --cpus-per-task=1 
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=4:00:00
+#SBATCH --job-name=phylogeny
+
+module load mafft/7.481-g3ca 
+module load trimal/1.4.1-wan5
+module load iqtree/1.6.12
+
+fasta=$1
+aln=$(echo ${fasta} | sed 's/.fasta/.msa/')
+trim=${aln}.trim
+
+mafft --auto $fasta > $aln
+trimal -automated1 -in $aln -out $trim
+iqtree -s $1 -m MF
+#iqtree -s $1 -m K2P+R2 -bb 10000
